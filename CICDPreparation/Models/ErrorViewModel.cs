48c6b85dce9ed811a03c0bad4d575dfb381631d0
hassanmemon231 <hassan17.aug@gmail.com>
2025-12-18 18:35:29 +0500

    changes

---
+++ CICDPreparation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CICDPreparation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
